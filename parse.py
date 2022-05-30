404a51e034617d0999813d2b15f4f6deae8622b8
Tommy Rochussen <rochussent@gmail.com>
2022-05-30 14:42:54 +0100

    completed parser.error

--- parse.py
+++ parse.py
@@ -671,9 +671,10 @@ class Parser:
 
     def error(self, msg, expect_next_list):
         self.end_of_file = False
-        print(self.scanner.show_error(self.symbol))
-        print(f"ERROR at index TBD!!: " + msg +
+        carat_msg, line_num, col_num = self.scanner.show_error(self.symbol)
+        print(f"ERROR on line {line_num} index {col_num}: " + msg +
               f", received {self.strSymbol()}")
+        print(carat_msg)
         self.error_count += 1
         while True:
             while self.symbol.id != self.scanner.SEMICOLON:
