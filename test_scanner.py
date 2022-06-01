fe71f8ed8955d7e376d8fe68982dd745673252c2
Tommy Rochussen <rochussent@gmail.com>
2022-06-01 10:58:16 +0100

    created some pytest fixtures

--- test_scanner.py
+++ test_scanner.py
@@ -9,3 +9,21 @@ def new_scanner(path):
     new_names = Names()
     new_scanner = Scanner(path, new_names)
     return new_scanner
+
+@pytest.fixture
+def empty_scanner():
+    """Return a Scanner object for an empty file"""
+    path = "test_files/scanner_test_files/empty_file.txt"
+    return new_scanner(path)
+
+@pytest.fixture
+def whitespace_scanner():
+    """Return a Scanner object for file with lots of whitespace"""
+    path = "test_files/scanner_test_files/whitespacey.txt"
+    return new_scanner(path)
+
+@pytest.fixture
+def comment_scanner():
+    """Return a Scanner object for file with only comments"""
+    path = "test_files/scanner_test_files/comment.txt"
+    return new_scanner(path)
