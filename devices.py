97c8f35477a8e8e07409555da10cd13a51867c10
Tommy Rochussen <rochussent@gmail.com>
2022-06-02 22:31:53 +0100

    implemented NOT gate in devices.py

--- devices.py
+++ devices.py
@@ -100,7 +100,7 @@ class Devices:
 
         self.devices_list = []
 
-        gate_strings = ["AND", "OR", "NAND", "NOR", "XOR"]
+        gate_strings = ["AND", "OR", "NAND", "NOR", "XOR", "NOT"]
         device_strings = ["CLOCK", "SWITCH", "DTYPE"]
         dtype_inputs = ["CLK", "SET", "CLEAR", "DATA"]
         dtype_outputs = ["Q", "QBAR"]
@@ -112,7 +112,7 @@ class Devices:
         self.signal_types = [self.LOW, self.HIGH, self.RISING,
                              self.FALLING, self.BLANK] = range(5)
         self.gate_types = [self.AND, self.OR, self.NAND, self.NOR,
-                           self.XOR] = self.names.lookup(gate_strings)
+                           self.XOR, self.NOT] = self.names.lookup(gate_strings)
         self.device_types = [self.CLOCK, self.SWITCH,
                              self.D_TYPE] = self.names.lookup(device_strings)
         self.dtype_input_ids = [self.CLK_ID, self.SET_ID, self.CLEAR_ID,
@@ -305,7 +305,13 @@ class Devices:
 
         elif device_kind in self.gate_types:
             # Device property is the number of inputs
-            if device_kind == self.XOR:
+            if device_kind == self.NOT:
+                if device_property is not None:
+                    error_type = self.QUALIFIER_PRESENT
+                else:
+                    self.make_gate(device_id, device_kind, 1)
+                    error_type = self.NO_ERROR
+            elif device_kind == self.XOR:
                 if device_property is not None:
                     error_type = self.QUALIFIER_PRESENT
                 else:
